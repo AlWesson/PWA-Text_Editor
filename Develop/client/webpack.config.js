363a235b490049ad079b755245f043a0efe0831a
Al <awessonn@gmail.com>
2023-06-16 12:13:39 -0700

    edited registerRoute using imports.

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -52,6 +52,24 @@ module.exports = () => {
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.m?js$/,
+          excluse: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                '@babel/plugin-proposal-object-rest-spread',
+                '@babel/transform-runtime'
+              ]
+            }
+          }
+        }
         
       ],
     },
